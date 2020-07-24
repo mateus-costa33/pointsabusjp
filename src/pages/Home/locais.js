b1006fd47d39c3ca43707f24aed73cf0a24e3fdc
Mateus Costa dos Santos <santos.costa@academico.ifpb.edu.br>
2020-07-23 23:45:15 -0300

    card-alteration

--- src/pages/Home/locais.js
+++ src/pages/Home/locais.js
@@ -11,7 +11,7 @@ const Locais = () => {
                 <div className="card-img-overlay">
                     <h5 className="card-title">Igreja São Francisco</h5>
                     <p className="card-text">Os ônibus adequados para chegar neste local: todos que vão para o Parque Solon de Lucena, a Lagoa. Quando chegar lá utilize o Google Maps para chegar no local mais rápido.</p>
-                    <a href="https://www.google.com.br/maps/place/Centro+Cultural+S%C3%A3o+Francisco/@-7.1149074,-34.8855109,17z/data=!3m1!4b1!4m5!3m4!1s0x7acdd49465ec499:0xf01081ffe9350fa8!8m2!3d-7.1149127!4d-34.8833222"><p className="card-text">Acesse aqui para ver a localização!</p></a>
+                    <a href="https://www.google.com.br/maps/place/Centro+Cultural+S%C3%A3o+Francisco/@-7.1149074,-34.8855109,17z/data=!3m1!4b1!4m5!3m4!1s0x7acdd49465ec499:0xf01081ffe9350fa8!8m2!3d-7.1149127!4d-34.8833222"><p className="card-text linkmaph">Acesse aqui para ver a localização!</p></a>
                 </div>
             </div>
         </div>
