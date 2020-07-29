0aacf9d2c0b401a2fff78718a193713185917574
Mateus Costa dos Santos <santos.costa@academico.ifpb.edu.br>
2020-07-29 00:29:19 -0300

    logo

--- src/pages/Home/container.js
+++ src/pages/Home/container.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import logo from '../../assets/logo-nova.png';
 import './styles.css';
 
 
@@ -7,6 +8,9 @@ class Container extends Component{
     render(){
         return(
             <div className="container mask">
+                <div className="text-center">
+                    <img src={logo} className="logo-nova" />
+                </div>
                 <h2 className="text-center title">Olá, segue os pontos do Roteiro Geoturistico do Centro Histórico de João Pessoa!</h2>
             </div>
         )
--- src/pages/Home/header.js
+++ src/pages/Home/header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from '../../assets/bus.png';
+import { FaBusAlt } from "react-icons/fa";
 
 
 const Header = () => {
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <img src={logo} className="logo" />
+                    <FaBusAlt className="icon" />
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -22,7 +22,7 @@ const Header = () => {
                         </li>
                         </ul>
                     </div>
-                </nav> 
+        </nav> 
     );
 }
 
