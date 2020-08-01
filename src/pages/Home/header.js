cb58a88aa5f51aa3097d2ba757db128a2dc42cf0
Mateus Costa dos Santos <santos.costa@academico.ifpb.edu.br>
2020-08-01 11:50:57 -0300

    Dark Mode - Light Mode

--- src/pages/Home/header.js
+++ src/pages/Home/header.js
@@ -11,14 +11,30 @@ import Footer from './footer';
 
 const Header = () => {
     const [darkMode, setDarkMode] = React.useState(getInitialMode());
+
     
     React.useEffect(() => {
         localStorage.setItem("dark", JSON.stringify(darkMode));
     }, [darkMode]);
 
     function getInitialMode() {
+        const isReturnuser = "dark" in localStorage;
         const savedMode = JSON.parse(localStorage.getItem("dark"));
+        const usePrefersDark = getPreferedMode();
+        if(isReturnuser){
+            return savedMode;
+        } else if (usePrefersDark){
+            return true
+        } else{
+            return false;
+        }
         return savedMode || false;
+    }
+
+    function getPreferedMode() {
+        if(!window.matchMedia) return;
+
+        return window.matchMedia("(prefers-color-scheme: dark)").matches;
     }
 
     return(
@@ -37,17 +53,27 @@ const Header = () => {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Locais de Interesse</a>
+                        <a className="nav-link" href="/">Locais de Interesse</a>
                     </li>
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Nosso Intinerário</a>
+                        <a className="nav-link" href="/Pointsabus">Nosso Intinerário</a>
                     </li>
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">Pegue um Busão!</a>
+                        <a className="nav-link" href="http://www.jampabus.com.br">Pegue um Busão!</a>
                     </li>
-                    <li className="nav-item active">
-                         <button className="btn text-right" onClick={() => setDarkMode(prevMode => !prevMode)}>Toogle Navigation</button>
-
+                    <li className="nav-item direction">
+                        <span style={{color: darkMode ? "#919190" : "#fcff45" , fontSize: 23, paddingRight: 5, fontWeight: "bold"}}>&#9788;</span>
+                    <label className="switch">
+                    <input
+                        checked={darkMode}
+                        onChange={() => setDarkMode(prevMode => !prevMode)}
+                        type="checkbox"
+                        className="checkbox"
+                        id="checkbox"
+                    />
+                        <span className="slider round"></span>
+                    </label>
+                    <span style={{color: darkMode ? "#3d00c9" : "#919190" , fontSize: 25, paddingLeft: 6, paddingTop:15, fontWeight: "bold"}}>&#9790;</span>
                     </li>
                     </ul>
                 </div>
